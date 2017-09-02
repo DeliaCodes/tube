8785ecc1fcc59f8eb213bbafdd93710bc4d8cb8d
deliacodes <deliacodes@gmail.com>
2017-09-01 18:57:24 -0700

    weird errors

--- search.js
+++ search.js
@@ -1,22 +1,29 @@
 "use strict";
+ function buildApiRequest(requestMethod, path, params) {
+    params = removeEmptyParams(params);
+    var request = gapi.client.request({
+          'method': requestMethod,
+          'path': path,
+          'params': params
+      });
 
   function defineRequest() {
 
-buildApiRequest('GET',
+buildApiRequest ('GET',
                 '/youtube/v3/search',
                 {'maxResults': '25',
                  'part': 'snippet',
                  'q': 'toFind',
                  'type': 'video'});
 
-  };
+  }};
 
 function displayResults () {};
 
 $('.search').click( function() {
 var toFind = $('input').val();
 
-getVideos(toFind);
+buildApiRequest(toFind);
 displayResults();
 
 });

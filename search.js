4ad7fa59072794fac073b0493148402c3676bc95
deliacodes <deliacodes@gmail.com>
2017-09-15 12:53:58 -0700

    some modifications. working on youtube api.

--- search.js
+++ search.js
@@ -6,6 +6,35 @@
  //make it clickable
  //search is hooked up but we need to finish the API auth first
 
+function start() {
+        // Initializes the client with the API key and the Translate API.
+        gapi.client.init({
+          'apiKey': 'YOUR_API_KEY',
+          'discoveryDocs': [''],
+        }).then(
+
+// Search for a specified string.
+function search() {
+  var q = $('input').val();
+  var request = gapi.client.youtube.search.list({
+    q: q,
+    part: 'snippet'
+  }).then(function(response) {
+          console.log(response.result.data.translations[0].translatedText);
+        }, function(reason) {
+          console.log('Error: ' + reason.result.error.message);
+        });
+      };
+
+  request.execute(function(response) {
+    var str = JSON.stringify(response.result);
+    $('#results').html('<pre>' + str + '</pre>');
+  });
+
+}
+
+search ();
+/*
 function displayResults () {};
 
 $('.search').click( function() {
@@ -36,6 +65,7 @@ buildApiRequest ('GET',
 
   }};
 
+*/
 
 
 

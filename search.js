5ce302a4012c299cbc08b7ce49f3c871f5885d98
Niaja <nfarve@mit.edu>
2017-09-17 20:21:10 -0400

    first working iteration

--- search.js
+++ search.js
@@ -10,58 +10,76 @@ function start() {
         // Initializes the client with the API key and the Translate API.
         gapi.client.init({
           'apiKey': 'AIzaSyB6nskjz1y78Vvihls3vxvnfQ7wllV2yFE',
-        }).then(
-
+        });
+      console.log("HI")
+}
 // Search for a specified string.
 function search() {
   var q = $('input').val();
-  var request = gapi.client.youtube.search.list({
-    q: q,
-    part: 'snippet'
+  console.log(gapi.client);
+  var request = gapi.client.request({
+    'path': 'https://www.googleapis.com/youtube/v3/search', 
+    'params': {'q': q, 'part': 'snippet'}
+  })
+  // var request = gapi.client.youtube.search.list({
+  //   q: q,
+  //   part: 'snippet'
+  // }
+  // );
+
+
+request.execute(function(response) {
+  console.log(response)
+  for (var i = 0; i < response.items.length; i++){
+    $('#results').append("<iframe width='420' height='315'\
+                          src=https://www.youtube.com/embed/"+response.items[i].id.videoId+"> \
+                          </iframe>")
   }
-  );
-}).then(
-
-  request.execute(function(response) {
-    var str = JSON.stringify(response.result);
-    $('#results').html('<pre>' + str + '</pre>');
-  };););
-};
+    // var str = JSON.stringify(response.items);
+    // $('#results').html('<pre>' + str + '</pre>');
+  });
+}
 // Loads the JavaScript client library and invokes `start` afterwards.
-      gapi.load('client', start);
+    
+function handleLoad(){
+  // 'youtube', 'v3'
+  gapi.load('client', start);
+}
 
-/*
-function displayResults () {};
+// function displayResults () {};
 
-$('.search').click( function() {
-var toFind = $('input').val();
-console.log(toFind);
-defineRequest(toFind);
-displayResults();
+$('#search-button').click( function() {
+  event.preventDefault();
+
+  search();
+  var toFind = $('input').val();
+  console.log(toFind);
+// defineRequest(toFind);
+// displayResults();
 
 });
 
 
- function buildApiRequest(requestMethod, path, params) {
-    params = removeEmptyParams(params);
-    var request = gapi.client.request({
-          'method': requestMethod,
-          'path': path,
-          'params': params
-      });
+//  function buildApiRequest(requestMethod, path, params) {
+//     params = removeEmptyParams(params);
+//     var request = gapi.client.request({
+//           'method': requestMethod,
+//           'path': path,
+//           'params': params
+//       });
 
-  function defineRequest(toFind) {
+//   function defineRequest(toFind) {
 
-buildApiRequest ('GET',
-                '/youtube/v3/search',
-                {'maxResults': '25',
-                 'part': 'snippet',
-                 'q': 'toFind',
-                 'type': 'video'});
+// buildApiRequest ('GET',
+//                 '/youtube/v3/search',
+//                 {'maxResults': '25',
+//                  'part': 'snippet',
+//                  'q': 'toFind',
+//                  'type': 'video'});
 
-  }};
+//   }};
 
-*/
+
 
 
 

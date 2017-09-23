73cdbe75b68c97682f5859f312136ff27305a7a6
deliacodes <deliacodes@gmail.com>
2017-09-22 18:14:18 -0700

    removed and simplified.

--- take2.js
+++ take2.js
@@ -2,41 +2,18 @@ var YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 //console log response! youtube id at the end of the url
 
-//do i need this?
-var RESULT_HTML_TEMPLATE = (
-  '<div>' +
-    '<h2>' +
-    '<a class="js-result-name" href="" target="_blank"></a> by <a class="js-user-name" href="" target="_blank"></a></h2>' +
-    '<p>Number of watchers: <span class="js-watchers-count"></span></p>' + 
-    '<p>Number of open issues: <span class="js-issues-count"></span></p>' +
-  '</div>'
-);
-
 function getDataFromApi(searchTerm, callback) {
   var query = {
     'path': 'https://www.googleapis.com/youtube/v3/search', 
     'params': {'key': 'AIzaSyB6nskjz1y78Vvihls3vxvnfQ7wllV2yFE','q': query, 'part': 'snippet'}
   }
-  console.log(query);
+  //console.log(query);
   $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
 }
-console.log(getDataFromApi);
 
-//do i need this?
-function renderResult(result) {
-  var template = $(RESULT_HTML_TEMPLATE);
-  template.find(".js-result-name").text(result.name).attr("href", result.html_url);
-  template.find(".js-user-name").text(result.owner.login).attr("href", result.owner.html_url);
-  template.find(".js-watchers-count").text(result.watchers_count);
-  template.find(".js-issues-count").text(result.open_issues);
-  return template;
-}
-
+//needs to be called and data passed to it
 function displaySearchData(data) {
-  var results = data.items.map(function(item, index) {
-    return renderResult(item);
-  });
-  $('#results').html(results);
+  $('#results').html('<pre>' +  + '</pre>')
 }
 
 function watchSubmit() {
@@ -44,10 +21,10 @@ function watchSubmit() {
     event.preventDefault(); //where is js-query?
     var queryTarget = $(event.currentTarget).find('.js-query');
     var query = $("input").val();
-    // clear out the input
+    // do I need queryTarget?
     queryTarget.val("");
     getDataFromApi(query, displaySearchData);
   });
 }
-console.log(watchSubmit);
+//console.log(watchSubmit);
 $(watchSubmit);
